0e9d125aa399ef9fabd2d8b8b262cb49a1bd39c5
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-12-03 18:19:57 -0500

    mongo init

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import { connect, Schema, model } from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const mongodb = process.env.CONNECTION_STRING;
+mongodb && connect(mongodb); //mongoose init
 
 const app: Application = express();
-
-const PORT = process.env.PORT || 5000;
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("It works!");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
