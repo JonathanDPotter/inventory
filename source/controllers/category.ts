fcd2e84a37941acce92922d426cf7b9ef0bbd5cf
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-12-07 15:33:10 -0500

    added authorization to create routes

--- source/controllers/category.ts
+++ source/controllers/category.ts
@@ -5,15 +5,33 @@ import Category from "../../models/categories";
 const NAMESPACE = "Category Controller";
 
 const createCategory = (req: Request, res: Response, next: NextFunction) => {
-  let { brand, name, price, description, onHand } = req.body;
+  const reject = () => {
+    res.setHeader("www-authenticate", "Basic");
+    res.sendStatus(401);
+  };
+
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return reject();
+  }
+
+  const [username, password] = Buffer.from(
+    authorization.replace("Basic ", ""),
+    "base64"
+  )
+    .toString()
+    .split(":");
+
+  if (!(username === "Jonathan" && password === process.env.AUTHORIZATION)) {
+    return reject();
+  }
+
+  const { name } = req.body;
 
   const category = new Category({
     _id: new mongoose.Types.ObjectId(),
-    brand,
     name,
-    price,
-    description,
-    onHand,
   });
 
   return category
--- source/controllers/sku.ts
+++ source/controllers/sku.ts
@@ -5,6 +5,28 @@ import Sku from "../../models/skus";
 const NAMESPACE = "Sku Controller";
 
 const createSku = (req: Request, res: Response, next: NextFunction) => {
+  const reject = () => {
+    res.setHeader("www-authenticate", "Basic");
+    res.sendStatus(401);
+  };
+
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return reject();
+  }
+
+  const [username, password] = Buffer.from(
+    authorization.replace("Basic ", ""),
+    "base64"
+  )
+    .toString()
+    .split(":");
+
+  if (!(username === "Jonathan" && password === process.env.AUTHORIZATION)) {
+    return reject();
+  }
+
   let { category, brand, name, price, description, onHand } = req.body;
 
   const sku = new Sku({
