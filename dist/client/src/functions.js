17f4279998c4323750d8539f66522aeaa26f40f8
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-12-30 13:16:38 -0500

    deploy

--- dist/client/src/functions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.capitalize = void 0;
-const capitalize = (name) => {
-    const first = name[0];
-    const rest = name.slice(1);
-    return first.toUpperCase() + rest;
-};
-exports.capitalize = capitalize;
