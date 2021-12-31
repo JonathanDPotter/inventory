5092daee4c39a7f6dbf5e8b52317e510f218901d
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-12-31 08:59:37 -0500

    minor change

--- source/server.ts
+++ source/server.ts
@@ -68,18 +68,19 @@ router.use("/api/categories", categoryRoutes);
 // serve static files if in production
 if (process.env.NODE_ENV === "production") {
   router.use(express.static("client/build"));
-
+  
+  router.get("/items/all", (req: Request, res: Response) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+  
   router.get("/*", (req: Request, res: Response) => {
     res.sendFile(
       path.resolve(__dirname, "..", "client", "build", "index.html")
     );
   });
 
-  router.get("/items/all", (req: Request, res: Response) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    );
-  });
 }
 
 // create server
