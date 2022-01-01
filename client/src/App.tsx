bd244d46bb5ae4f88c8d778a4701426cfb217259
JonathanDPotter <jonathan.d.potter@outlook.com>
2022-01-01 07:07:57 -0500

    changed BrowserRouter to HashRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getCategories, getItems } from "./api";
 import { addCat, addSku } from "./store";
